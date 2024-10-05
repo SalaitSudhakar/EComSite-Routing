09597990b2c4e452742aa63dea8987c49d44c090
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2024-10-05 10:07:16 +0530

    Discount added

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,12 +3,14 @@ import "./Cart.css";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 
-
 const Cart = ({ cartItems, addToCart, removeFromCart, updateQuantity }) => {
   const total = cartItems.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
+
+  const discount = total * 0.10;
+  const finalPrice = total - discount;
 
   return (
     <div className="p-4 w-full md:w-5/6 mx-auto min-h-screen">
@@ -30,8 +32,7 @@ const Cart = ({ cartItems, addToCart, removeFromCart, updateQuantity }) => {
                 </div>
               </div>
 
-               
-                <div className="flex space-x-4 border-2 bg-gray-50 p-1 rounded-md">
+              <div className="flex space-x-4 border-2 bg-gray-50 p-1 rounded-md">
                 <button
                   onClick={() => updateQuantity(item.id, item.quantity - 1)}
                   className="w-3 h-3 sm:w-4 sm:h-4 my-auto"
@@ -51,14 +52,14 @@ const Cart = ({ cartItems, addToCart, removeFromCart, updateQuantity }) => {
                 >
                   <FaPlus />
                 </button>
-                </div>
-              
-                <div>
-                    <span className="font-bold text-sm sm:text-lg text-black">Subtotal: </span>
-                    <span className="text-sm sm:text-lg text-green-800 bg-green-300 p-1 rounded-md border-0 font-bold">${(item.price * item.quantity).toFixed(2)}
-                  </span>
-                </div>
-             
+              </div>
+
+              <div>
+                <span className="font-bold text-sm sm:text-lg text-black">Subtotal: </span>
+                <span className="text-sm sm:text-lg text-black bg-yellow-300 p-1 rounded-md border-0 font-semibold">
+                  ${(item.price * item.quantity).toFixed(2)}
+                </span>
+              </div>
 
               <button
                 onClick={() => removeFromCart(item.id)}
@@ -66,13 +67,33 @@ const Cart = ({ cartItems, addToCart, removeFromCart, updateQuantity }) => {
               >
                 Remove
               </button>
-              
             </div>
-            
           ))}
+
+          {/* Total */}
           <div className="mt-4 flex justify-between items-center p-3 bg-slate-200 rounded-sm">
             <span className="text-xl sm:text-2xl font-bold">Total: </span>
-            <span className="bg-emerald-300 text-emerald-900 p-1 border-0 rounded-md font-bold">${total.toFixed(2)}</span>
+            <span className="bg-blue-300 text-blue-900 p-1 border-0 rounded-md font-bold">
+              ${total.toFixed(2)}
+            </span>
+          </div>
+
+         
+          {/* Discount */}
+          <div className="mt-4 flex justify-between items-center p-3 bg-slate-200 rounded-sm">
+            <span className="text-xl sm:text-2xl font-bold">Discount: </span>
+            <span className="bg-red-300 text-red-900 p-1 border-0 rounded-md font-bold">
+              - ${discount.toFixed(2)}
+            </span>
+          </div>
+
+          <hr className="border-1 border-gray-500 my-4" />
+          {/* Final Price */}
+          <div className="mt-4 flex justify-between items-center p-3 bg-slate-200 rounded-sm">
+            <span className="text-xl sm:text-2xl font-bold">Final Price: </span>
+            <span className="bg-green-300 text-green-900 p-1 border-0 rounded-md font-bold">
+              ${finalPrice.toFixed(2)}
+            </span>
           </div>
         </>
       )}
