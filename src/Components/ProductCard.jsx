72a1cf53bac027f70c87cdb4f36259fb625bb20b
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2024-10-05 10:19:30 +0530

    Cards hover style added

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -4,13 +4,13 @@ const ProductCard = ({ product, addToCart, cartItems }) => {
   const inCart = cartItems.some(item => item.id === product.id);
 
   return (
-    <div className="bg-white shadow-md rounded-md p-4">
+    <div className="bg-white shadow-md rounded-md p-4 hover:scale-105">
       <img src={product.image} alt={product.title} className="h-48 mx-auto" />
       <h3 className="text-lg font-bold mt-2">{product.title}</h3>
       <p className="text-sm mt-2">${product.price}</p>
       <button
         onClick={() => addToCart(product)}
-        className={`mt-4 w-full bg-${inCart ? 'red' : 'green'}-500 text-white py-2 rounded`}
+        className={`mt-4 w-full bg-${inCart ? 'red' : 'green'}-500 text-white py-2 rounded hover:bg-${inCart ? 'red': 'green'}-400`}
       >
         {inCart ? 'Remove from Cart' : 'Add to Cart'}
       </button>
